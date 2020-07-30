08ecb745c3cc800f0a5218f1c4f94da9df00e58a
LevX Team <team@levx.app>
2020-07-31 05:10:50 +0900

    Dynamically load versionCode and buildNumber in app.json

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,14 @@ import "dotenv/config";
 export default ({ config }) => {
     return {
         ...config,
+        android: {
+            ...config.android,
+            versionCode: Number(process.env.ANDROID_VERSION_CODE)
+        },
+        ios: {
+            ...config.ios,
+            buildNumber: process.env.IOS_BUILD_NUMBER
+        },
         extra: {
             twitterConsumerKey: process.env.TWITTER_CONSUMER_KEY,
             twitterConsumerSecret: process.env.TWITTER_CONSUMER_SECRET
